3ca75ce27ba6e748f8b071d780b2ab4a32f1c4ba
Matthew Gall <me@matthewgall.dev>
2024-01-26 21:33:18 +0000

    Added /api/data.json caching to KV

--- functions/api/data.json.js
+++ functions/api/data.json.js
@@ -1,9 +1,22 @@
 import Fuel from '../../classes/Fuel';
 
 export async function onRequest(context) {
+    let obj = await context.env.KV.get("fueldata-json");
+    if (obj !== null) {
+        return new Response(obj, {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
+    }
+
     let f = new Fuel;
     let d = await f.getData();
 
+    // Next, we save the data to KV
+    await context.env.KV.put('fueldata-json', JSON.stringify(resp), {expirationTtl: 21600})
+    
     return new Response(JSON.stringify(d), {
         headers: {
             "Content-Type": "application/json",
