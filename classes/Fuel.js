351674e433ae9d63f2855c89d962052dab8372bc
Matthew Gall <me@matthewgall.dev>
2024-01-26 14:56:34 +0000

    Added new Fuel class, and setup API function to use it

---
+++ classes/Fuel.js
@@ -0,0 +1,38 @@
+import Feeds from '../feeds.json'
+
+export default class Fuel {
+    async getData() {
+        let data = {}
+
+        // Now we iterate through the feeds and download them all
+        for (let f of Object.keys(Feeds)) {
+            let d = await fetch(Feeds[f], {
+                cf: { cacheTtlByStatus: { "200-299": 86400, 404: 1, "500-599": 0 } },
+            })
+    
+            d = await d.json();
+    
+            // Now, we iterate for each station to get the prices
+            for (let stn of d.stations) {
+                try {
+                    data[f][stn.site_id] = {}
+                }
+                catch(e) {
+                    data[f] = {}
+                    data[f][stn.site_id] = {}
+                }
+    
+                data[f][stn.site_id] = {
+                    'address': {
+                        'brand': stn.brand,
+                        'address': stn.address,
+                        'postcode': stn.postcode
+                    },
+                    'location': stn.location,
+                    'prices': stn.prices
+                }
+            }
+        }
+        return data
+    }
+}
--- functions/api/data.mapbox.js
+++ functions/api/data.mapbox.js
@@ -1,5 +1,38 @@
-import Feeds from './feeds.json'
+import Fuel from '../../classes/Fuel';
 
-export function onRequest(context) {
-    return new Response(JSON.stringify(Feeds), { headers: { 'Content-Type': 'application/json' }})
+export async function onRequest(context) {
+    let resp = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+    let f = new Fuel;
+    let d = await f.getData();
+
+    for (let brand of Object.keys(d)) {
+        for (let s of Object.keys(d[brand])) {
+            let stn = d[brand][s];
+            let prices = [];
+            for (let fuel of Object.keys(stn.prices)) {
+                prices.push(`<strong>${fuel}</strong> ${stn.prices[fuel]}`);
+            }
+            resp.features.push({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [stn.location.longitude, stn.location.latitude]
+                },
+                "properties": {
+                    "title": `${stn.address.brand}, ${stn.address.postcode}`,
+                    "description": prices.join("<br />")
+                }
+            });
+        }
+    }
+    
+    return new Response(JSON.stringify(resp), {
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+    });
 }
