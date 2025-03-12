46cb5f469620a30b8efe9c4cfe75ee3cbe9e642e
Matthew Gall <me@matthewgall.dev>
2025-03-12 00:22:34 +0000

    Made mirror.mjs simpler, and supported the right format for R2 data

--- mirror.mjs
+++ mirror.mjs
@@ -9,21 +9,25 @@ let axiosConfig = {
         'User-Agent': 'fuelaround.me/builder'
     }
 }
+let fueldata = {}
 
 async function downloadLists() {
     // Next, we download all the feeds
     for (let t of Object.keys(feeds)) {
         await axios.get(feeds[t], axiosConfig).then(function (resp) {
             if(resp.status == 200) {
-                let data = resp.data
-                
-                // Once we have the data, we're going to write it to a file
-                fs.writeFileSync(`data/${t}.json`, JSON.stringify(data));
+                // Next, we append it to our data
+                fueldata[t] = resp.data
             }
         }).catch(function (error) {})
     }
+    // And return it
+    return fueldata;
 }
 
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
-    let data = await downloadLists()
+    let data = await downloadLists();
+
+    // And write it to our data folder
+    fs.writeFileSync(`data/fueldata.json`, JSON.stringify(data));
 }
