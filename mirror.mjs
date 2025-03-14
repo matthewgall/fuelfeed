b3e7f9eafe7930decffab10d37ee29fde2f7781b
Matthew Gall <me@matthewgall.dev>
2025-03-14 09:34:18 +0000

    Updated mirror.mjs to dump the raw data to R2, to allow src/fuel.ts to do the conversion

--- mirror.mjs
+++ mirror.mjs
@@ -16,38 +16,13 @@ async function downloadLists() {
     for (let t of Object.keys(feeds)) {
         await axios.get(feeds[t], axiosConfig).then(function (resp) {
             if(resp.status == 200) {
-                // Now, we do some iteration magic to format it correctly
-                let data = resp.data
-                for (let stn of data.stations) {
-                    try {
-                        fueldata[t][stn.site_id] = {}
-                    }
-                    catch(e) {
-                        fueldata[t] = {}
-                        fueldata[t][stn.site_id] = {}
-                    }
-        
-                    fueldata[t][stn.site_id] = {
-                        'address': {
-                            'brand': stn.brand,
-                            'address': stn.address,
-                            'postcode': stn.postcode
-                        },
-                        'location': stn.location,
-                        'prices': stn.prices,
-                        'updated': data.last_updated
-                    }
-                }
+                // Now, we write the data to our data folder
+                fs.writeFileSync(`data/${t}.json`, JSON.stringify(resp.data));
             }
         }).catch(function (error) {})
     }
-    // And return it
-    return fueldata;
 }
 
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
-    let data = await downloadLists();
-
-    // And write it to our data folder
-    fs.writeFileSync(`data/fueldata.json`, JSON.stringify(data));
+    await downloadLists();
 }
