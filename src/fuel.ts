122c02d426b79f1538e214bae082a0e8f768cc94
Matthew Gall <me@matthewgall.dev>
2025-03-30 23:12:54 +0000

    We use R2 first, before anything else, HTTP lookups aren't free

--- src/fuel.ts
+++ src/fuel.ts
@@ -7,27 +7,28 @@ export default class Fuel {
         // Now we iterate through the feeds and download them all
         for (let f of Object.keys(Feeds)) {
             try {
-                let d: any = await fetch(Feeds[f], {
-                    headers: {
-                        'Accept': 'application/json',
-                        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-                    },
-                    cf: {
-                        cacheTtlByStatus: {
-                            "200-299": 1800,
-                            404: 1,
-                            "500-599": 0
-                        }
-                    },
-                })
-        
-                if (d.status !== 200) {
-                    // If we didn't get success, we'll try to grab the latest from R2
-                    let r2: any = await env.R2.get(`${f}.json`)
-                    if (r2 !== null) d = await r2.json();
-                }
-                else {
-                    d = await d.json();
+                let d: any = null;
+
+                // First, we try and save ourselves a HTTP lookup, by using R2
+                let r2: any = await env.R2.get(`${f}.json`)
+                if (r2 !== null) d = await r2.json();
+
+                // Now, and only if we couldn't find it in R2
+                if (d === null) {
+                    d = await fetch(Feeds[f], {
+                        headers: {
+                            'Accept': 'application/json',
+                            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+                        },
+                        cf: {
+                            cacheTtlByStatus: {
+                                "200-299": 1800,
+                                404: 1,
+                                "500-599": 0
+                            }
+                        },
+                    })
+                    if (d.status == 200) d = await d.json();
                 }
                 
                 // Now, we iterate for each station to get the prices
