72fa1de79c63a94422d4061ffcdb50d6f20342e7
Matthew Gall <me@matthewgall.dev>
2025-04-06 20:39:25 +0000

    Try and register a service worker, with periodic update

--- public/js/worker.js
+++ public/js/worker.js
@@ -20,3 +20,17 @@ self.addEventListener("fetch", fetchEvent => {
         })
     )
 })
+
+self.addEventListener("periodicsync", (event) => {
+    if (event.tag === "update-prices") {
+      event.waitUntil(updateNews());
+    }
+});
+
+async function registerPeriodicSync() {
+    const swRegistration = await navigator.serviceWorker.ready;
+    swRegistration.periodicSync.register("update-prices", {
+      // try to update every 6 hours
+      minInterval: 6 * 60 * 60 * 1000,
+    });
+  }
